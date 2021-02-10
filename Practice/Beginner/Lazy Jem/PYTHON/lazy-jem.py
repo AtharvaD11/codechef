9bb6f22d2decc7f07504da2651c6e1c2ae70f421
AtharvaD11 <67207001+AtharvaD11@users.noreply.github.com>
2021-02-10 12:40:42 +0530

    Create lazy-jem.py

---
+++ Practice/Beginner/Lazy Jem/PYTHON/lazy-jem.py
@@ -0,0 +1,26 @@
+"""
+    AUTHOR - Atharva Deshpande
+    GITHUB - https://github.com/AtharvaD11
+    QUESTION LINK  - https://www.codechef.com/problems/TALAZY
+"""
+
+for _ in range(int(input())):
+    n,b,m = map(int,input().split())
+    total_min = 0
+    #total_min += n//2 -1
+    while(n):
+        if n%2!=0:
+            quo = (n+1)//2
+            rem = n-quo
+            total_min += quo*m + b
+            n = rem
+            m *= 2
+        else:
+            quo = n//2
+            rem = n-quo
+            total_min += quo*m + b
+            n = rem
+            m *= 2
+    print(total_min-b)
+
+
